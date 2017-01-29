a981d03d213f765fb9de0b1d76e53d945445f6e3
Selma Delgado <selmadelgado@gmail.com>
2017-01-29 13:36:07 -0600

    Add Event Listeners to Roll Dice

--- app/temp/app.js
+++ app/temp/app.js
@@ -44,17 +44,30 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	/*
-	GAME RULES:
+	'use strict';
 
-	- The game has 2 players, playing in rounds
-	- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-	- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-	- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-	- The first player to reach 100 points on GLOBAL score wins the game
+	var scores, roundScore, activePlayer;
 
-	*/
-	"use strict";
+	scores = [0, 0];
+	roundScore = 0;
+	activePlayer = 1;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score__1').textContent = '0';
+	document.getElementById('score__2').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('current-2').textContent = '0';
+
+	document.querySelector('.btn__roll').addEventListener('click', function () {
+
+	  var dice = Math.floor(Math.random() * 6) + 1;
+	  console.log(dice);
+
+	  var diceDOM = document.querySelector('.dice');
+	  diceDOM.style.display = 'block';
+	  diceDOM.src = 'assets/images/dice-' + dice + '.png';
+	});
 
 /***/ }
 /******/ ]);
