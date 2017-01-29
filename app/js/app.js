eab3f459f948896805c627a2aac0ad045002b1e9
Selma Delgado <selmadelgado@gmail.com>
2017-01-29 17:14:20 -0600

    Update Scores and Change Active Player

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,6 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-
 document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score__1').textContent = '0';
@@ -22,5 +21,20 @@ document.querySelector('.btn__roll').addEventListener('click', function(){
   diceDOM.style.display = 'block';
   diceDOM.src = 'assets/images/dice-' + dice + '.png';
 
+  if(dice !== 1){
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 1 ? activePlayer = 2: activePlayer = 1;
+    roundScore = 0;
+    
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('current-2').textContent = '0';
+
+    document.querySelector('.player-1-panel').classList.toggle('player--active');
+    document.querySelector('.player-2-panel').classList.toggle('player--active');
+    
+    document.querySelector('.dice').style.display = 'none';
+  }
   
 });
--- app/temp/app.js
+++ app/temp/app.js
@@ -67,6 +67,22 @@
 	  var diceDOM = document.querySelector('.dice');
 	  diceDOM.style.display = 'block';
 	  diceDOM.src = 'assets/images/dice-' + dice + '.png';
+
+	  if (dice !== 1) {
+	    roundScore += dice;
+	    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	  } else {
+	    activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
+	    roundScore = 0;
+
+	    document.getElementById('current-1').textContent = '0';
+	    document.getElementById('current-2').textContent = '0';
+
+	    document.querySelector('.player-1-panel').classList.toggle('player--active');
+	    document.querySelector('.player-2-panel').classList.toggle('player--active');
+
+	    document.querySelector('.dice').style.display = 'none';
+	  }
 	});
 
 /***/ }
