361146d23c824d8c1ec7f7cc1e453ff51a59ccb8
Selma Delgado <selmadelgado@gmail.com>
2017-01-31 15:18:24 -0600

    Implement Hold Function and Display Winner

--- app/temp/app.js
+++ app/temp/app.js
@@ -72,18 +72,37 @@
 	    roundScore += dice;
 	    document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	  } else {
-	    activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
-	    roundScore = 0;
-
-	    document.getElementById('current-1').textContent = '0';
-	    document.getElementById('current-2').textContent = '0';
-
-	    document.querySelector('.player-1-panel').classList.toggle('player--active');
-	    document.querySelector('.player-2-panel').classList.toggle('player--active');
-
-	    document.querySelector('.dice').style.display = 'none';
+	    nextPlayer();
 	  }
 	});
+
+	document.querySelector('.btn__hold').addEventListener('click', function () {
+	  scores[activePlayer - 1] += roundScore;
+
+	  document.querySelector('#score__' + activePlayer).textContent = scores[activePlayer - 1];
+
+	  if (scores[activePlayer - 1] >= 20) {
+	    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+	    document.querySelector('.dice').style.display = 'none';
+	    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+	    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	  } else {
+	    nextPlayer();
+	  }
+	});
+
+	function nextPlayer() {
+	  activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
+	  roundScore = 0;
+
+	  document.getElementById('current-1').textContent = '0';
+	  document.getElementById('current-2').textContent = '0';
+
+	  document.querySelector('.player-1-panel').classList.toggle('player--active');
+	  document.querySelector('.player-2-panel').classList.toggle('player--active');
+
+	  document.querySelector('.dice').style.display = 'none';
+	}
 
 /***/ }
 /******/ ]);
